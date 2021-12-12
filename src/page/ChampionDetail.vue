ce30fe95a1dd73da3a40073b9b582291abf2fac6
zyxeeker <190474450@qq.com>
2021-12-12 22:42:31 +0800

    :white_check_mark: 符文&英雄详情页面构建

---
+++ src/page/ChampionDetail.vue
@@ -0,0 +1,115 @@
+<template>
+  <div style="background-color: black;display: flex;height:100%">
+    <div class="bg bg-blur"></div>
+    <div class="content content-front">
+      <div style="position:inherit;height:100%;width:100%;display: flex;justify-content: center;align-items: center;">
+        <div class="bg bg-img"></div>
+        <div
+            style="padding: 0 150px;top:20%;color:#42b983;position: absolute;display: flex;height: 100%;flex-direction: column;justify-content: center;">
+
+          <span class="title">不灭狂雷</span>
+
+          <div style="display: flex;align-items: center">
+            <svg width="530" height="1" viewBox="0 0 530 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="530" height="1" transform="matrix(1 0 0 -1 0 1)" fill="#C4C4C4" fill-opacity="0.5"/>
+            </svg>
+            <span class="name">沃利贝尔</span>
+            <svg width="530" height="1" viewBox="0 0 530 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="530" height="1" transform="matrix(1 0 0 -1 0 1)" fill="#C4C4C4" fill-opacity="0.5"/>
+            </svg>
+          </div>
+
+          <div style="display: flex">
+            <div style="display: flex;flex-direction: column;align-items: center;">
+              <p style="color: white;width:75%;word-break: break-word;text-align: center">
+                大多数人都认为身为霜卫首领的丽桑卓是一个活着的圣人，为弗雷尔卓德的各族带来了智慧与治愈。但事实却非如此，她使用元素魔法将臻冰扭曲成黑暗的物质并用它来冰封或穿刺胆敢揭露她隐藏的秘密的人。她的过去可能是符文之地末日的起始。</p>
+              <svg width="595" height="1" viewBox="0 0 595 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="595" height="1" fill="#C4C4C4" fill-opacity="0.5"/>
+              </svg>
+              <!--   相关信息    -->
+            </div>
+
+            <!--            <svg width="1" height="420" viewBox="0 0 1 420" fill="none" xmlns="http://www.w3.org/2000/svg">-->
+            <!--              <rect width="1" height="420" fill="#C4C4C4" fill-opacity="0.5"/>-->
+            <!--            </svg>-->
+
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChampionDetail",
+  setup() {
+    return {
+      bk: "https://cdragon.valhir.cn/latest/champion/106/splash-art/skin/000",
+      bk1: "https://cdragon.valhir.cn/latest/champion/106/splash-art/skin/000"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bg {
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000913 73.44%), url('https://cdragon.valhir.cn/latest/champion/106/splash-art/skin/000');
+  height: 100%;
+  /*text-align: center;*/
+  /*background: ;*/
+}
+
+.bg-blur {
+  float: left;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  filter: blur(15px);
+}
+
+.bg-img {
+  float: left;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.content-front {
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.title {
+  width: 100%;
+  text-align: center;
+  font-family: YouSheBiaoTiHei;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 50px;
+  line-height: 65px;
+  /* identical to box height */
+
+  letter-spacing: 0.15em;
+
+  color: #FFFFFF;
+}
+
+.name {
+  width: 65%;
+  text-align: center;
+  font-family: OPPOSans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 120px;
+  line-height: 158px;
+
+  color: #FFFFFF;
+}
+</style>
---
+++ src/page/Item.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="wrap" :style="{'background-image':'url('+bk+')'}">
+    <div class="wrap-mask">
+      <div
+          style="position:absolute;height:100vh;top:60px;right:-76px;display:flex;flex-direction: column;justify-content: center;z-index: 99">
+        <span class="v-slogan">ITEM</span>
+      </div>
+      <div style="display:flex;justify-content: space-between;align-items: center;position: absolute;left: 0;top: 0;">
+        <div style="height:155px;width: 300px;display: flex;flex-direction: column;align-items: center;">
+          <span class="patch_title">装备</span>
+          <div style="display: flex;flex-direction: row;position: relative;left: 45px;">
+            <span class="eng_des">LEGENDARY<br>MYTHIC</span>
+            <span class="zh_des" style="margin-right: 10px;">传<br>说</span>
+            <span class="zh_des">神<br>话</span>
+          </div>
+          <Button bk="#0BC6E3" bk-hover="#25DEFC" title="了解更多" style="position:absolute;top: 170%;left: 50%;"/>
+        </div>
+        <span class="_des">你<br>会<br>选<br>择<br>怎<br>样<br>的<br>装<br>备<br>与<br>对<br>手<br>抗<br>衡<br>？</span>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button";
+
+export default {
+  name: "Item",
+  components: {Button},
+  data() {
+    return {
+      bk: require('@/assets/item_bk.png')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrap {
+  height: 100%;
+  width: 100%;
+  background: no-repeat top center;
+  background-size: cover;
+}
+
+.wrap-mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.patch_title {
+  font-family: TsangerYuMo;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 100px;
+  line-height: 125px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  text-align: center;
+  color: white;
+}
+
+.v-slogan {
+  height: 84px;
+  width: auto;
+  font-family: Maler;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 30px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 1.8em;
+
+  color: #C4C4C4;
+
+  transform: rotate(90deg);
+}
+
+.eng_des {
+  position: relative;
+  bottom: -90px;
+  left: -40px;
+  transform: rotate(90deg);
+
+  font-family: Maler;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 48px;
+  line-height: 58px;
+  align-items: center;
+
+  color: rgba(255, 255, 255, 0.5);
+}
+
+.zh_des {
+  position: relative;
+  left: -120px;
+  top: -5px;
+
+  font-family: TsangerYuMo;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 48px;
+  line-height: 100%;
+  /* or 48px */
+
+  display: flex;
+  align-items: center;
+
+  color: #FFFFFF;
+}
+
+._des {
+  position: relative;
+  top: 125px;
+
+  font-family: SYST;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 19px;
+  line-height: 23px;
+
+  color: #FFFFFF;
+}
+
+</style>
